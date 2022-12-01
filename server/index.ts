bbe1912e742ce9f7d06e62317c8c36946319cc66
Lee Suan <leesuan97@gmail.com>
2022-12-01 15:46:52 -0800

    added finnhub api

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,24 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+const finnhub = require("finnhub");
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
+const api_key = finnhub.ApiClient.instance.authentications["api_key"];
+api_key.apiKey = process.env.FINNHUB_TOKEN;
+const finnhubClient = new finnhub.DefaultApi();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get("/api/quote", (req: Request, res: Response) => {
+  console.log(finnhubClient);
+  res.send("connected");
 });
 
 app.listen(port, () => {
